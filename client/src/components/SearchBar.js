6fce2b612c6da8f6563e189125fcb14534e48a7d
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-04-22 17:51:08 -0400

    Update SearchBar.js
    still working with props

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,18 +1,22 @@
-import React from "react"
+import React from "react";
 
-const SearchBar = (props) => {
+const SearchBar = ( ) => {
     return (
-        <div className = "search-bar">
-            <form onSubmit = {props.SearchBook} action = "">
-                <input onChange = {props.handleSearch} type = "text" /> 
-                <button type = "submit"> Search </button>
-                <select defaultValue = "sort" onChange = {props.handleSort}>
-                    <option disabled value = "newest"> Newest </option>
-                    <option value = "oldest"> Oldest </option>
-                </select>    
-            </form>
-        </div>
-    )
-}
+      <div className="search-bar">
+        <form onSubmit={props.SearchBook} action="">
+          <input onChange={props.handleSearch} type="text" />
+          <button type="submit"> Search </button>
+          <select defaultValue="sort" onChange={props.handleSort}>
+            <option disabled value="newest">
+              {" "}
+              Newest{" "}
+            </option>
+            <option value="oldest"> Oldest </option>
+          </select>
+        </form>
+      </div>
+    );
+  }
 
-export default SearchBar
+
+export default SearchBar;
