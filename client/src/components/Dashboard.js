2e9d4a597d652d3b518faaa79919fd62596faf0d
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-04-22 17:53:18 -0400

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,26 +1,14 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
-import Userfront from "@userfront/react";
-import TeacherAnswer from "./teacherAnswer";
-import QandA from "./qAndA";
-import BookApp from "../BookApp"; 
+import BookCard from "./BookCard";
 
-Userfront.init("xbpm8jmn");
-
-function Dashboard({ location }) {
-    if (!Userfront.accessToken()) {
-      return <Navigate to={{ pathname: "/login", state: { from: location } }} />;
-    }
-    const userData = JSON.stringify(Userfront.user, null, 2);
+const BookList = (props) => {
     return (
-      <div>
-        <h2>Welcome, Young Reader: <span>{Userfront.user.name}</span></h2>
-        {/* <QandA user={Userfront.user.name}/> */}
-        {/* <TeacherAnswer /> */}
-        <BookApp />
-        <button onClick={Userfront.logout}>Logout</button>
+      <div className="list">
+        {props.books.map((book, i) => {
+          return <BookCard />;
+        })}
       </div>
     );
   }
 
-  export default Dashboard
+export default BookList;
