d25e3dfd87ea341a605651155da04c0e7027e5d1
Jasmin Halkic <jasmin.halkic0@gmail.com>
2022-04-24 14:39:17 -0400

    added different dashboard view depending on user role

---
+++ client/src/components/BookSearch.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function BookSearch() {
+  const [search, setSearch] = useState({ searchResults: ""});
+  console.log(search)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { isbn } = event.target.elements;
+
+    let url =
+      "https://www.googleapis.com/books/v1/volumes?q=isbn:" +
+      isbn.value +
+      "&key=AIzaSyAJsd4HV0TTDwY5R5239XL99bKLdiZWxcw";
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setSearch({ searchResults: result.items });
+        console.log(result.items[0].volumeInfo.title);
+        console.log(result.items[0].volumeInfo.authors[0]);
+      });
+      isbn.value = ""
+  };
+  return (
+    <div>
+        <h2>Before you begin reading a new book, add it here!</h2>
+        <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="isbn">ISBN:</label>
+        <input type="number" id="isbn" required />
+        <button type="submit">Submit</button>
+        <h2> Title: {search.searchResults[0]?.volumeInfo.title}</h2>
+        <h2> Author: {search.searchResults[0]?.volumeInfo.authors[0]}</h2>
+      </form>
+    </div>
+  );
+}
+
+export default BookSearch;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,28 +1,48 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Userfront from "@userfront/react";
-import TeacherAnswer from "./teacherAnswer"
+import TeacherAnswer from "./teacherAnswer";
 import StudentAnswer from "./studentAnswer";
-import QandA from "./qAndA"
-import Books from './Books'
+import QandA from "./qAndA";
+import Books from "./Books";
+import BookSearch from "./BookSearch.js"
 
 Userfront.init("xbpm8jmn");
 
 function Dashboard({ location }) {
-    if (!Userfront.accessToken()) {
-      return <Navigate to={{ pathname: "/login", state: { from: location } }} />;
-    }
-    const userData = JSON.stringify(Userfront.user, null, 2);
+  if (!Userfront.accessToken()) {
+    return <Navigate to={{ pathname: "/login", state: { from: location } }} />;
+  }
+  const userData = JSON.stringify(Userfront.user, null, 2);
+
+  if (Userfront.user.hasRole("admin")) {
     return (
       <div>
-        <h2>Welcome, Young Reader: <span>{Userfront.user.name}</span></h2>
+        <h2>
+          Welcome, Young Reader: <span>{Userfront.user.name}</span>
+        </h2>
         {/* <QandA user={Userfront.user.name}/> */}
+        <TeacherAnswer />
+        {/* <StudentAnswer user={Userfront.user.name} /> */}
+        {/* <Books /> */}
+        <button onClick={Userfront.logout}>Logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>
+          Welcome, Young Reader: <span>{Userfront.user.name}</span>
+        </h2>
+        <BookSearch />
+        <QandA user={Userfront.user.name}/>
         {/* <TeacherAnswer /> */}
-        <StudentAnswer user={Userfront.user.name}/>
+        <StudentAnswer user={Userfront.user.name} />
         {/* <Books /> */}
         <button onClick={Userfront.logout}>Logout</button>
       </div>
     );
   }
+}
 
-  export default Dashboard
+export default Dashboard;
