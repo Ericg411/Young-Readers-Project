83764c04eeb19a6eace4cea616b7857e81a41176
Cindy Marcelle <88116236+cmarcelle@users.noreply.github.com>
2022-04-25 20:17:42 -0400

    updated css file

--- client/src/components/BookSearch.js
+++ client/src/components/BookSearch.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./styles/App.css";
+import "./styles/index.css";
 
 function BookSearch() {
   const [search, setSearch] = useState({ searchResults: ""});
